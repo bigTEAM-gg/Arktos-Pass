333754fda4a28d018e5c19c78767e1a57f59d03d
andreyvarlamov <35208582+andreyvarlamov@users.noreply.github.com>
2024-08-02 23:16:11 -0700

    Bypass tree culling

--- scenes/TreeCuller.gd
+++ scenes/TreeCuller.gd
@@ -8,12 +8,13 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var enviro = get_tree().get_nodes_in_group('enviro')
+	pass
+	#var enviro = get_tree().get_nodes_in_group('enviro')
 	
-	for e: Node3D in enviro:
-		if e.global_position.distance_squared_to(global_position) > 2000:
-			e.visible = false
-			e.process_mode = Node.PROCESS_MODE_DISABLED
-		else:
-			e.visible = true
-			e.process_mode = Node.PROCESS_MODE_INHERIT
+	#for e: Node3D in enviro:
+		#if e.global_position.distance_squared_to(global_position) > 2000:
+			#e.visible = false
+			#e.process_mode = Node.PROCESS_MODE_DISABLED
+		#else:
+			#e.visible = true
+			#e.process_mode = Node.PROCESS_MODE_INHERIT
